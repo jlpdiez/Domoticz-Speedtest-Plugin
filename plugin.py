8ccde50a43a19a1a480124e7ea7e3d012f79fc74
Xorfor <32778657+Xorfor@users.noreply.github.com>
2018-01-14 10:57:57 +0100

    Added imgaes and speedtest now runs as subprocess. So not blocking other hardware anymore

--- plugin.py
+++ plugin.py
@@ -19,22 +19,25 @@
 </plugin>
 """
 import Domoticz
+import subprocess
 import os
-
-_HEARTBEATS = 6  # minutes
-
-_COMMAND = "speedtest-cli"
-_OPTIONS = "--simple"
-_PING = "Ping"
-_DOWNLOAD = "Download"
-_UPLOAD = "Upload"
-
-_PING_UNIT = 1
-_DOWNLOAD_UNIT = 2
-_UPLOAD_UNIT = 3
 
 
 class BasePlugin:
+
+    __HEARTBEATS2MIN = 6
+
+    # Command
+    __COMMAND = "speedtest-cli"
+    __OPTIONS = "--simple"
+    # Device units
+    __UNIT_PING = 1
+    __UNIT_DOWNLOAD = 2
+    __UNIT_UPLOAD = 3
+    # Search strings
+    __PING = "Ping"
+    __DOWNLOAD = "Download"
+    __UPLOAD = "Upload"
 
     def __init__(self):
         self.__config_ok = False
@@ -53,22 +56,27 @@ class BasePlugin:
             self.__polling = 1
         #
         # Check for the existence of the command
-        ret = os.popen("dpkg -l | grep " + _COMMAND).read()
-        pos = ret.find(_COMMAND)
+        ret = os.popen("dpkg -l | grep " + self.__COMMAND).read()
+        pos = ret.find(self.__COMMAND)
         if pos >= 0:
             self.__config_ok = True
         else:
             self.__config_ok = False
-            Domoticz.Error(_COMMAND + " not found")
+            Domoticz.Error(self.__COMMAND + " not found")
             return
         #
+        # Images
+        # Check if images are in database
+        if "xfr_speedtest" not in Images:
+            Domoticz.Image("xfr_speedtest.zip").Create()
+        image = Images["xfr_speedtest"].ID
+        Domoticz.Debug("Image created. ID: "+str(image))
+        # Create devices
         if (len(Devices) == 0):
-            Domoticz.Device(Unit=_PING_UNIT, Name=_PING, TypeName="Custom", Options={"Custom": "1;ms"}, Used=1).Create()
-            Domoticz.Device(Unit=_DOWNLOAD_UNIT, Name=_DOWNLOAD, TypeName="Custom", Options={"Custom": "1;Mb/s"},
-                            Used=1).Create()
-            Domoticz.Device(Unit=_UPLOAD_UNIT, Name=_UPLOAD, TypeName="Custom", Options={"Custom": "1;Mb/s"},
-                            Used=1).Create()
-
+            Domoticz.Device(Unit=self.__UNIT_PING, Name=self.__PING, TypeName="Custom", Options={"Custom": "1;ms"}, Image=image, Used=1).Create()
+            Domoticz.Device(Unit=self.__UNIT_DOWNLOAD, Name=self.__DOWNLOAD, TypeName="Custom", Options={"Custom": "1;Mb/s"}, Image=image, Used=1).Create()
+            Domoticz.Device(Unit=self.__UNIT_UPLOAD, Name=self.__UPLOAD, TypeName="Custom", Options={"Custom": "1;Mb/s"}, Image=image, Used=1).Create()
+        # Log config
         DumpConfigToLog()
 
     def onStop(self):
@@ -102,8 +110,9 @@ class BasePlugin:
         #
         self.__runAgain -= 1
         if self.__runAgain <= 0:
+            self.__runAgain = self.__polling * self.__HEARTBEATS2MIN
             # Run command
-            retList = os.popen(_COMMAND + " " + _OPTIONS).read().splitlines()
+            retList = subprocess.Popen([self.__COMMAND, self.__OPTIONS], stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip().splitlines()
             num = 0
             for ret in retList:
                 cmd[num], value[num], units[num] = ret.split(" ")
@@ -112,16 +121,14 @@ class BasePlugin:
             for num in range(len(cmd)):
                 Domoticz.Debug(
                     "num: " + str(num) + " - cmd: " + cmd[num] + " - value: " + value[num] + " - units: " + units[num])
-                if cmd[num].find(_PING) >= 0:
-                    UpdateDevice(_PING_UNIT, int(float(value[num])), value[num] + " " + units[num])
-                if cmd[num].find(_DOWNLOAD) >= 0:
-                    UpdateDevice(_DOWNLOAD_UNIT, int(float(value[num])), value[num] + " " + units[num])
-                if cmd[num].find(_UPLOAD) >= 0:
-                    UpdateDevice(_UPLOAD_UNIT, int(float(value[num])), value[num] + " " + units[num])
-            self.__runAgain = self.__polling * _HEARTBEATS
+                if cmd[num].find(self.__PING) >= 0:
+                    UpdateDevice(self.__UNIT_PING, int(float(value[num])), str(round(float(value[num]), 2)) + " " + units[num])
+                if cmd[num].find(self.__DOWNLOAD) >= 0:
+                    UpdateDevice(self.__UNIT_DOWNLOAD, int(float(value[num])), value[num] + " " + units[num])
+                if cmd[num].find(self.__UPLOAD) >= 0:
+                    UpdateDevice(self.__UNIT_UPLOAD, int(float(value[num])), value[num] + " " + units[num])
         else:
             Domoticz.Debug("onHeartbeat called, run again in " + str(self.__runAgain) + " heartbeats.")
-
 
 global _plugin
 _plugin = BasePlugin()
@@ -167,16 +174,30 @@ def DumpConfigToLog():
             Domoticz.Debug("'" + x + "':'" + str(Parameters[x]) + "'")
     Domoticz.Debug("Device count: " + str(len(Devices)))
     for x in Devices:
-        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
-        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
-        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
-        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
-        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
-        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+        Domoticz.Debug("Unit...........: " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Debug("Idx............: " + str(Devices[x].ID))
+        Domoticz.Debug("Name...........: " + Devices[x].Name)
+        Domoticz.Debug("Type...........: " + str(Devices[x].Type) + " / " + str(Devices[x].SubType) )
+        Domoticz.Debug("nValue.........: " + str(Devices[x].nValue))
+        Domoticz.Debug("sValue.........: " + Devices[x].sValue)
+        Domoticz.Debug("LastLevel......: " + str(Devices[x].LastLevel))
+    for x in Settings:
+        Domoticz.Debug("Setting........: " + str(x) + " - " + str(Settings[x]))
 
 def UpdateDevice(Unit, nValue, sValue, TimedOut=0, AlwaysUpdate=False):
     # Make sure that the Domoticz device still exists (they can be deleted) before updating it
     if Unit in Devices:
         if Devices[Unit].nValue != nValue or Devices[Unit].sValue != sValue or Devices[Unit].TimedOut != TimedOut or AlwaysUpdate:
             Devices[Unit].Update(nValue=nValue, sValue=str(sValue), TimedOut=TimedOut)
-            Domoticz.Debug("Update " + Devices[Unit].Name + ": " + str(nValue) + " - '" + str(sValue) + "'")
+            Domoticz.Debug("Update " + Devices[Unit].Name + ": " + str(nValue) + " - '" + str(sValue) + "' - " + str(TimedOut))
+
+def DumpHTTPResponseToLog(httpDict):
+    if isinstance(httpDict, dict):
+        Domoticz.Debug("HTTP Details ("+str(len(httpDict))+"):")
+        for x in httpDict:
+            if isinstance(httpDict[x], dict):
+                Domoticz.Debug("....'"+x+" ("+str(len(httpDict[x]))+"):")
+                for y in httpDict[x]:
+                    Domoticz.Debug("........'" + y + "':'" + str(httpDict[x][y]) + "'")
+            else:
+                Domoticz.Debug("....'" + x + "':'" + str(httpDict[x]) + "'")
